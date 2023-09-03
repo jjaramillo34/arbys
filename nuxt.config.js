9d4f9a324e49e3f130653557d02a26b48ff45b24
Javier Jaramillo <jjaramillo34@gmail.com>
2023-09-03 10:53:17 -0400

    [chore]: update google analytics ID

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,29 @@
 export default {
-    // Target: https://go.nuxtjs.dev/config-target
-    target: 'static',
+  // Target: https://go.nuxtjs.dev/config-target
+  target: "static",
 
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'Arby\'s Puerto Rico',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Welcome to Arby\'s Puerto Rico! We Have the Meats!' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-        ],
-        __dangerouslyDisableSanitizers: ['script'],
-        script: [
-            {
-                hid: 'order-delivery-button',
-                innerHTML: `
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "Arby's Puerto Rico",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Welcome to Arby's Puerto Rico! We Have the Meats!",
+      },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+    __dangerouslyDisableSanitizers: ["script"],
+    script: [
+      {
+        hid: "order-delivery-button",
+        innerHTML: `
                 (function(e, t, r, n) {
                     var o, c, s;
                     o = e.document, c = t.children[0], s = o.createElement("script"), e.StorefrontSDKObject = "StorefrontSDK", e[e.StorefrontSDKObject] = {
@@ -43,56 +45,54 @@ export default {
                     buttonText: 'Order Pickup & Delivery',
                     })
                 `,
-            },
-            {
-                hid: 'gtag-manager',
-                src: `https://www.googletagmanager.com/gtag/js?id=G-BW88H2GGSM`,
-                // window.dataLayer = window.dataLayer || [];
-                // function gtag(){dataLayer.push(arguments);}
-                // gtag('js', new Date());
-                // gtag('config', 'G-BW88H2GGSM');`
-                defer: true
-            },
-            {
-                hid: 'gtm-script2',
-                innerHTML: `
+      },
+      {
+        hid: "gtag-manager",
+        src: `https://www.googletagmanager.com/gtag/js?id=G-BW88H2GGSM`,
+        // window.dataLayer = window.dataLayer || [];
+        // function gtag(){dataLayer.push(arguments);}
+        // gtag('js', new Date());
+        // gtag('config', 'G-BW88H2GGSM');`
+        defer: true,
+      },
+      {
+        hid: "gtm-script2",
+        innerHTML: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
           
-                  gtag('config', 'UA-111111111-1');
+                  gtag('config', 'G-J7NF3XLX2Z');
                 `,
-                type: 'text/javascript',
-                charset: 'utf-8'
-            }
-        ]
-    },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [
-        "~/assets/css/tailwind.css"
+        type: "text/javascript",
+        charset: "utf-8",
+      },
     ],
+  },
 
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["~/assets/css/tailwind.css"],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-        // https://go.nuxtjs.dev/tailwindcss
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/google-analytics',
-    ],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [],
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-analytics",
+  ],
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {},
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
 
-    googleAnalytics: {
-        id: 'G-RSMPXRJ0RZ'
-    }
-}
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  googleAnalytics: {
+    id: "G-J7NF3XLX2Z",
+  },
+};
