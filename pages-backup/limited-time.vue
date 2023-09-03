075e25ec1cdd97cf20a15e14ce85bc9a6a34612e
Javier Jaramillo <jjaramillo34@gmail.com>
2023-09-03 12:46:21 -0400

    [chore]: update next gen images

--- pages-backup/limited-time.vue
+++ pages-backup/limited-time.vue
@@ -1,8 +1,10 @@
 <template>
-    <Layout>
-        <div class="container mx-auto px-5">
-            <div class="grid grid-cols-1 sm:grid-cols-2 relative z-10 gap-5 lg:pt-10 py-24 items-center">
-                  <!-- <div>
+  <Layout>
+    <div class="container mx-auto px-5">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 relative z-10 gap-5 lg:pt-10 py-24 items-center"
+      >
+        <!-- <div>
                       <img src="~/assets/images/limited-mint-choco.jpg" class="object-scale-down">
                   </div>
                   <div>
@@ -14,16 +16,25 @@
                   <div>
                       <img src="~/assets/images/limited-time-chicken-cheddar-ranch.jpg" class="object-scale-down">
                   </div> -->
-                  
-                  <div>
-                      <img src="~/assets/images/limited-nueva-batida.jpg" class="object-scale-down">
-                  </div>
-                  <div>
-                      <img src="~/assets/images/limited-tripple-decker.jpg" class="object-scale-down">
-                  </div>
-            </div>
+
+        <div>
+          <img
+            src="~/assets/images/limited-nueva-batida.webp"
+            class="object-scale-down"
+            alt="Nueva batida de chocolate"
+          />
+          >
         </div>
-    </Layout>
+        <div>
+          <img
+            src="~/assets/images/limited-tripple-decker.webp"
+            class="object-scale-down"
+            alt="Triple Decker"
+          />
+        </div>
+      </div>
+    </div>
+  </Layout>
 </template>
 
 <script>
